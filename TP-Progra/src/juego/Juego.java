9371fdf79b1020b735359ee4d8100db56de8f182
GRampazzo <93059046+GRampazzo@users.noreply.github.com>
2022-05-04 20:29:08 -0300

    Add files via upload

---
+++ TP-Progra/src/juego/Juego.java
@@ -0,0 +1,39 @@
+package juego;
+
+import entorno.Entorno;
+import entorno.InterfaceJuego;
+
+public class Juego extends InterfaceJuego {
+
+	// El objeto Entorno que controla el tiempo y otros
+	private Entorno entorno;
+	
+	public Juego() {
+		// Inicializa el objeto entorno
+		this.entorno = new Entorno(this, "Sakura Ikebana Delivery - Grupo N - Apellido1 - Apellido2 -Apellido3 - V0.01", 800, 600);
+		
+		// Inicializar lo que haga falta para el juego
+		// ...
+
+		// Inicia el juego!
+		this.entorno.iniciar();
+	}
+
+	/**
+	 * Durante el juego, el método tick() será ejecutado en cada instante y 
+	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
+	 * actualizar el estado interno del juego para simular el paso del tiempo 
+	 * (ver el enunciado del TP para mayor detalle).
+	 */
+	public void tick() {
+		// Procesamiento de un instante de tiempo
+		// ...
+		
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Juego juego = new Juego();
+	}
+
+}
