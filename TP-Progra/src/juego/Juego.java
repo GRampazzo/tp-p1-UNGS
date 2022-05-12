3023e2aed630eb83988fb09565fe75ba817a750e
GRampazzo <gastonrampazzo@gmail.com>
2022-05-12 20:16:37 -0300

    Update(ColisionObs)

--- TP-Progra/src/juego/Juego.java
+++ TP-Progra/src/juego/Juego.java
@@ -1,8 +1,7 @@
 package juego;
 
 import java.util.Random;
-import java.awt.Color;
-
+import java.awt.*;
 import entorno.Entorno;
 import entorno.InterfaceJuego;
 
@@ -12,17 +11,18 @@ public class Juego extends InterfaceJuego {
 	private Entorno entorno;
 	private Mikasa mikasa;
 	private Tiempo tiempo;
-	private Suero suero;
 	private int cantObs;
-	private Obstaculos [] obs;
-	
+	private Obstaculos[] obs;
+	private Suero suero;
+
 	public Juego() {
 		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this, "Sakura Ikebana Delivery - Grupo 12 - Huayta - Rampazzo -Apellido3 - V0.01", 800, 600);
-		this.mikasa = new Mikasa(400, 300, 40, 40,0);
+		this.entorno = new Entorno(this, "Sakura Ikebana Delivery - Grupo 12 - Huayta - Rampazzo -Apellido3 - V0.01",
+				800, 600);
+		this.mikasa = new Mikasa(400, 300, 40, 40, 0);
 		this.tiempo = new Tiempo(true, 0);
 		this.suero = new Suero(0, 0, 0);
-		this.cantObs=5;
+		this.cantObs = 5;
 		this.iniciarObs();
 		// Inicializar lo que haga falta para el juego
 		// ...
@@ -32,10 +32,10 @@ public class Juego extends InterfaceJuego {
 	}
 
 	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y 
-	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
-	 * actualizar el estado interno del juego para simular el paso del tiempo 
-	 * (ver el enunciado del TP para mayor detalle).
+	 * Durante el juego, el método tick() será ejecutado en cada instante y por lo
+	 * tanto es el método más importante de esta clase. Aquí se debe actualizar
+	 * el estado interno del juego para simular el paso del tiempo (ver el enunciado
+	 * del TP para mayor detalle).
 	 */
 	public void tick() {
 		// Procesamiento de un instante de tiempo
@@ -43,73 +43,94 @@ public class Juego extends InterfaceJuego {
 		if (tiempo.isInicia()) {
 			if (entorno.estaPresionada('w'))
 				mikasa.moverAdelante();
-		
+
 			if (entorno.estaPresionada('a'))
 				mikasa.moverIzquierda();
-		
+
 			if (entorno.estaPresionada('d'))
 				mikasa.moverDerecha();
-			
+
+			if (entorno.estaPresionada('s'))
+				mikasa.moverAtras();
+
+			mikasa.dibujar(entorno);
+			mikasa.colisionPantalla();
+
 			if (suero.colision(mikasa.getX(), mikasa.getY(), suero.getX(), suero.getY(), suero.getRadio())) {
 				suero.setX(0);
 				suero.setY(0);
 				suero.setRadio(0);
 			}
-			
-			//apariciones del suero			
+
+			// apariciones del suero
 			if (tiempo.getContar() == 0.015 || (int) tiempo.getContar() == 19) {
 				suero.setX(Math.random() * 800);
 				suero.setY(Math.random() * 600);
 				suero.setRadio(30);
 			}
-			
-			//dibujar
-			mikasa.dibujar(entorno);
-			
+
 			if (((int) tiempo.getContar() > 10 && (int) tiempo.getContar() < 18)
-			 || ((int) tiempo.getContar() > 20 && (int) tiempo.getContar() < 28)) {
+					|| ((int) tiempo.getContar() > 20 && (int) tiempo.getContar() < 28)) {
 				suero.dibujar(entorno);
 			}
-		
-			for (Obstaculos o: this.obs) {
+
+			for (Obstaculos o : this.obs) {
 				o.dibujar(entorno);
+				chocar(o);
 			}
-			
-			//cronometro
+
+			entorno.cambiarFont("Arial", 22, Color.WHITE);
+			entorno.escribirTexto("angulo:" + mikasa.getAngulo(), 50, 50);
+			entorno.escribirTexto("y: " + mikasa.getY(), 50, 110);
+			entorno.escribirTexto("x: " + mikasa.getX(), 50, 80);
+			entorno.escribirTexto("y-alto: " + (mikasa.getY() - mikasa.getAlto()), 50, 170);
+			entorno.escribirTexto("x+ancho: " + (mikasa.getX() + mikasa.getAncho()), 50, 140);
+			// cronometro
 			tiempo.setContar(0.015);
-			
-			//si queres controlar el tiempo en el eclipse:
-			//System.out.println(tiempo.getContar());
-			
-			//marca fin del juego
-			if ((int) tiempo.getContar() > 40) {
-				this.tiempo = new Tiempo(false, 0);
-			}
-		} else {
-			entorno.cambiarFont("Arial", 32, Color.WHITE);
-			entorno.escribirTexto("GAME OVER", 300, 300);
-		}
-		
+
+			// si queres controlar el tiempo en el eclipse:
+			System.out.println(tiempo.getContar());
+
+			// marca fin del juego
+			/*
+			 * if (tiempo.getContar() >= 30 && tiempo.getContar() <= 31) { this.tiempo = new
+			 * Tiempo(false, 0); } } else { entorno.cambiarFont("Arial", 32, Color.WHITE);
+			 * entorno.escribirTexto("GAME OVER", 300, 300);
+			 */}
+
 	}
+
 	private void iniciarObs() {
 		Obstaculos[] lista = new Obstaculos[this.cantObs];
-		Random r=new Random();
+		Random r = new Random();
 		int xPiso = 370;
 		int yPiso = 500;
-		int ancho = 80;
-		int alto = 80;
+		int ancho = 60;
+		int alto = 60;
 		for (int i = 0; i < this.cantObs; i++) {
-			Obstaculos p = new Obstaculos (xPiso, yPiso, ancho, alto);
+			Obstaculos p = new Obstaculos(xPiso, yPiso, ancho, alto);
 			lista[i] = p;
 			yPiso = yPiso - 100;
 			xPiso = r.nextInt(600);
-			if (xPiso>=350 && xPiso<450) {
-				xPiso=r.nextInt(600);
+			if (xPiso >= 300 && xPiso <= 450) {
+				xPiso = r.nextInt(600);
 			}
 		}
 		this.obs = lista;
 	}
-	
+
+	public boolean colision(double x1, double y1, double x2, double y2, double dist) {
+		return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < dist * dist;
+	}
+
+	public void chocar(Obstaculos o) {
+		for (int i = 0; i < this.obs.length; i++) {
+			if (colision(this.obs[i].getX(), this.obs[i].getY(), mikasa.getX(), mikasa.getY(), 60)) {
+				mikasa.colisionObs(o.getX() - 1, o.getY() + 1, o.getAncho() + 1, o.getAlto() - 1);
+			}
+		}
+	}
+
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Juego juego = new Juego();
--- TP-Progra/src/juego/Mikasa.java
+++ TP-Progra/src/juego/Mikasa.java
@@ -10,6 +10,8 @@ public class Mikasa {
 	private double ancho;
 	private double alto;
 	private double angulo;
+	private double velocidad;
+	private int estado;
 
 	public Mikasa(double x, double y, double ancho, double alto, double angulo) {
 		this.x = x;
@@ -17,6 +19,8 @@ public class Mikasa {
 		this.ancho = ancho;
 		this.alto = alto;
 		this.angulo = angulo;
+		this.velocidad=1.5;
+		this.estado=0;
 	}
 
 	public void dibujar(Entorno e) {
@@ -25,25 +29,12 @@ public class Mikasa {
 
 	// movimiento
 	public void moverAdelante() {
-		if (this.x > 20 && ((this.x + this.ancho) < 800) && this.y < 590 && ((this.y - this.alto) > 0)) {
-			this.x += Math.cos(this.angulo) * 1.5;
-			this.y += Math.sin(this.angulo) * 1.5;
-		}
-		if(this.x <= 20){
-			this.x = this.x+10;
-		}
-		if ((this.x + this.ancho) >= 800){	
-			this.x= this.x-10;
-		}
-		if (this.y>=580) {
-			this.y =this.y-10; 
-		}
-		if ((this.y-this.alto) <= 0){
-			this.y= this.y+10;
+		if(this.estado==0) {
+			this.x += Math.cos(this.angulo) * velocidad;
+			this.y += Math.sin(this.angulo) * velocidad;
 		}
 	}
-
-
+	
 	public void moverDerecha() {
 		this.angulo += 0.08;
 	}
@@ -51,5 +42,94 @@ public class Mikasa {
 	public void moverIzquierda() {
 		this.angulo -= 0.08;
 	}
+
+	public void moverAtras() {
+		if (this.estado==0){
+			this.x -= Math.cos(this.angulo) * velocidad;
+			this.y -= Math.sin(this.angulo) * velocidad;
+		}
+	}
+	public void colisionPantalla() {
+		if (this.x <= 20) {
+			this.x = this.x + 2;
+		}
+		if ((this.x + this.ancho) >= 820) {
+			this.x = this.x - 2;
+		}
+		if (this.y >= 580) {
+			this.y = this.y - 2;
+		}
+		if ((this.y - this.alto) <= -20) {
+			this.y = this.y + 2;
+		}
+	}
 	
+	public void colisionObs(double x, double y, double ancho, double alto) {
+		if (this.x <= x+ancho) {
+			this.estado = 1;
+			this.x=this.x+2;
+		}
+		if (this.x + this.ancho >= x) {
+			this.estado=1;
+			this.x = this.x - 2;
+		}
+		
+		if (this.y >= y-alto) {
+			this.estado=1;
+			this.y = this.y - 2;
+		}
+		if (this.y - this.alto <= y) {
+			this.estado=1;
+			this.y = this.y + 2;
+		}
+		this.estado=0;
+	}
+
+
+	public boolean colision(double xIzq, double xDer, double yAba, double yArr) {
+		boolean tocandoX = xIzq <= this.x + this.ancho && this.x <= xDer;
+		boolean tocandoY = yAba >= this.y + this.alto && this.y >= yArr;
+		return tocandoX && tocandoY;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(double ancho) {
+		this.ancho = ancho;
+	}
+
+	public double getAlto() {
+		return alto;
+	}
+
+	public void setAlto(double alto) {
+		this.alto = alto;
+	}
+
+	public double getAngulo() {
+		return angulo;
+	}
+
+	public void setAngulo(double angulo) {
+		this.angulo = angulo;
+	}
+
 }
