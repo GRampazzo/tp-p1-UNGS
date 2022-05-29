2348daf5710bf0a0de7a636dcb5104a5c570fad8
KevDtto <104948198+KevDtto@users.noreply.github.com>
2022-05-29 17:26:23 -0300

    Update Juego.java

--- TP-Progra/src/juego/Juego.java
+++ TP-Progra/src/juego/Juego.java
@@ -30,7 +30,7 @@ public class Juego extends InterfaceJuego {
 	private int eliminados;
 	private Clip sonido;
 	private Clip sonido2;
-	
+
 	public Juego() {
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Sakura Ikebana Delivery - Grupo N - Apellido1 - Apellido2 -Apellido3 - V0.01",
@@ -68,30 +68,26 @@ public class Juego extends InterfaceJuego {
 		// ...
 
 		if (tiempo.isInicia()) {
-			
-			if ((int) tiempo.getTimer()==0) {
-				tiempo.setInicia(false);
-			}
 			this.sonido.start();
 //			dibujar
 			entorno.dibujarImagen(this.imageFondo, 400, 400, 0, 1.7);
-			mikasa.dibujarSprite(entorno);
+			this.mikasa.dibujarSprite(entorno);
 
 //			instrucciones
 			if (entorno.estaPresionada('w') || entorno.estaPresionada(entorno.TECLA_ARRIBA))
-				mikasa.moverAdelante();
+				this.mikasa.moverAdelante();
 
 			if (entorno.estaPresionada('a') || entorno.estaPresionada(entorno.TECLA_IZQUIERDA))
-				mikasa.moverIzquierda();
+				this.mikasa.moverIzquierda();
 
 			if (entorno.estaPresionada('s') || entorno.estaPresionada(entorno.TECLA_ABAJO))
-				mikasa.moverAtras();
+				this.mikasa.moverAtras();
 
 			if (entorno.estaPresionada('d') || entorno.estaPresionada(entorno.TECLA_DERECHA))
-				mikasa.moverDerecha();
+				this.mikasa.moverDerecha();
 
 //			titanes
-			if (titanes.largo() < 6) {
+			if (this.titanes.largo() < 6) {
 				crearTitan();
 			}
 			
@@ -104,7 +100,7 @@ public class Juego extends InterfaceJuego {
 
 //			disparo
 			if (this.entorno.estaPresionada(this.entorno.TECLA_ESPACIO)
-					&& this.mikasa.getUltimoDisparo() + 50 <= this.tiempo.getContar() && !mikasa.convertir) {
+					&& this.mikasa.getUltimoDisparo() + 50 <= this.tiempo.getContar() && !this.mikasa.isConvertir()) {
 				this.mikasa.disparar((int) this.tiempo.getContar());
 			}
 			
@@ -120,43 +116,42 @@ public class Juego extends InterfaceJuego {
 //			y la distancia entre los circulos es menor a 100, resta o suma en sus respectivos X e Y. del titan
 			for (Obstaculos2 obs : this.obstaculos.getObstaculos2()) {
 			this.titanes.forEachNodo(ttn -> {
-				ttn.getElemento().distancia(ttn.getElemento().getX(), ttn.getElemento().getY(), mikasa.getX(),
-						mikasa.getY());
+				ttn.getElemento().distancia(ttn.getElemento().getX(), ttn.getElemento().getY(), this.mikasa.getX(),
+						this.mikasa.getY());
 				obstaculos.distancia(obs.getX(), obs.getY(), ttn.getElemento().getX(), ttn.getElemento().getY());
-				if (obstaculos.colisiona(obs.getRadio(), ttn.getElemento().getRadio(), obstaculos.getDistancia())
-						&& obstaculos.getDistancia() <= 100) {
+				if (obstaculos.colisiona(obs.getRadio(), ttn.getElemento().getRadio(), this.obstaculos.getDistancia())
+						&& this.obstaculos.getDistancia() <= 100) {
 					titanChocaObs(ttn.getElemento(), obs);
 				}
-				if (!mikasa.isConvertir() && ttn.getElemento().colisiona(ttn.getElemento().getRadio(),
-						mikasa.getRadio(), ttn.getElemento().getDistancia())
+				if (!this.mikasa.isConvertir() && ttn.getElemento().colisiona(ttn.getElemento().getRadio(),
+						this.mikasa.getRadio(), ttn.getElemento().getDistancia())
 						&& ttn.getElemento().getDistancia() <= 50) {
-					tiempo.setInicia(false);
+					this.tiempo.setInicia(false);
 				}
-				if (mikasa.isConvertir() && ttn.getElemento().colisiona(ttn.getElemento().getRadio(),
-						mikasa.getRadio(), ttn.getElemento().getDistancia())
+				if (this.mikasa.isConvertir() && ttn.getElemento().colisiona(ttn.getElemento().getRadio(),
+						this.mikasa.getRadio(), ttn.getElemento().getDistancia())
 						&& ttn.getElemento().getDistancia() <= 100) {
-					mikasaChocaTtn(ttn);
+					this.mikasaChocaTtn(ttn);
 				}
 
 				obs.dibujarSprite(entorno);
-				ttn.getElemento().dibujar2(entorno);
+				ttn.getElemento().dibujarSprite(entorno);
 				return null;
 			});
-//				obs.dibujar2(entorno);
 		}
 			
 			
 //			lo mismo pero con mikasa
 			for (Obstaculos2 obs : this.obstaculos.getObstaculos2()) {
-			obstaculos.distancia(obs.getX(), obs.getY(), mikasa.getX(), mikasa.getY());
-			if (!mikasa.isConvertir()
-					&& obstaculos.colisiona(obs.getRadio(), mikasa.getRadio(), obstaculos.getDistancia())
-					&& obstaculos.getDistancia() <= 70) {
+			this.obstaculos.distancia(obs.getX(), obs.getY(), this.mikasa.getX(), this.mikasa.getY());
+			if (!this.mikasa.isConvertir()
+					&& this.obstaculos.colisiona(obs.getRadio(), this.mikasa.getRadio(), this.obstaculos.getDistancia())
+					&& this.obstaculos.getDistancia() <= 70) {
 				mikasaChocaObs(obs);
-			} else if (mikasa.isConvertir()
-					&& obstaculos.colisiona(obs.getRadio(), mikasa.getRadio(), obstaculos.getDistancia())
-					&& obstaculos.getDistancia() <= 85) {
-				mikasaChocaObs(obs);
+			} else if (this.mikasa.isConvertir()
+					&& this.obstaculos.colisiona(obs.getRadio(), this.mikasa.getRadio(), this.obstaculos.getDistancia())
+					&& this.obstaculos.getDistancia() <= 85) {
+				this.mikasaChocaObs(obs);
 			}
 		}
 				
@@ -164,20 +159,18 @@ public class Juego extends InterfaceJuego {
 				impactoObs(obs);
 			}
 
-//			controla el fin del juego
-//			if ((int) tiempo.getContar() > 40) {
-//				this.tiempo = new Tiempo(false, 0);
-//			}
+			this.tiempo.setContar(1);
+			this.tiempo.setTimer(0.01111102);
 
-//			tiempo.setContar(0.015);
-			tiempo.setContar(1);
-			tiempo.setTimer(0.01111102);
-
+			if ((int) tiempo.getTimer()==0) {
+				tiempo.setInicia(false);
+			}
+			
 //			diujar datos
 			entorno.cambiarFont("Arial Black", 25, Color.BLACK);
 			entorno.escribirTexto("KYOJINES ELIMINADOS: " + 0, 4, 588);
 			entorno.cambiarFont("Arial Black", 25, Color.WHITE);
-			entorno.escribirTexto("TIEMPO: " + (int) tiempo.getTimer(), 620, 23);
+			entorno.escribirTexto("TIEMPO: " + (int) this.tiempo.getTimer(), 620, 23);
 			entorno.escribirTexto("PUNTAJE: " + this.puntos, 4, 23);
 
 		} else {
@@ -206,13 +199,13 @@ public class Juego extends InterfaceJuego {
 			ttn.setY(ttn.getY() + 2);
 		}
 	}
-	
+
 	public <T> void mikasaChocaTtn(Nodo<T> ttn) {
 		this.titanes.forEachNodo(nodoTitan -> {
-			if (ttn.getId() == nodoTitan.getId() && mikasa.isConvertir()) {
+			if (ttn.getId() == nodoTitan.getId() && this.mikasa.isConvertir()) {
 				this.titanes.quitarPorId(nodoTitan.getId());
-				mikasa.setConvertir(false);
-				mikasa.setRadio(40);
+				this.mikasa.setConvertir(false);
+				this.mikasa.setRadio(40);
 				this.puntos += 50;
 				this.eliminados += 1;
 			}
@@ -220,63 +213,61 @@ public class Juego extends InterfaceJuego {
 		});
 	}
 
-}
+
 
 	public void mikasaChocaObs(Obstaculos2 obs) {
-		if (mikasa.getX() <= obs.getX() && mikasa.getY() <= obs.getY()) {
-			mikasa.setX(mikasa.getX() - 2);
-			mikasa.setY(mikasa.getY() - 2);
-		} else if (mikasa.getX() >= obs.getX() && mikasa.getY() >= obs.getY()) {
-			mikasa.setX(mikasa.getX() + 2);
-			mikasa.setY(mikasa.getY() + 2);
-		} else if (mikasa.getX() >= obs.getX() && mikasa.getY() <= obs.getY()) {
-			mikasa.setX(mikasa.getX() + 2);
-			mikasa.setY(mikasa.getY() - 2);
-		} else if (mikasa.getX() <= obs.getX() && mikasa.getY() >= obs.getY()) {
-			mikasa.setX(mikasa.getX() - 2);
-			mikasa.setY(mikasa.getY() + 2);
+		if (this.mikasa.getX() <= obs.getX() && this.mikasa.getY() <= obs.getY()) {
+			this.mikasa.setX(this.mikasa.getX() - 2);
+			this.mikasa.setY(this.mikasa.getY() - 2);
+		} else if (this.mikasa.getX() >= obs.getX() && this.mikasa.getY() >= obs.getY()) {
+			this.mikasa.setX(this.mikasa.getX() + 2);
+			this.mikasa.setY(this.mikasa.getY() + 2);
+		} else if (this.mikasa.getX() >= obs.getX() && this.mikasa.getY() <= obs.getY()) {
+			this.mikasa.setX(this.mikasa.getX() + 2);
+			this.mikasa.setY(this.mikasa.getY() - 2);
+		} else if (this.mikasa.getX() <= obs.getX() && this.mikasa.getY() >= obs.getY()) {
+			this.mikasa.setX(this.mikasa.getX() - 2);
+			this.mikasa.setY(this.mikasa.getY() + 2);
 		}
 	}
 
 	public void crearSuero() {
 		for (Obstaculos2 obs : this.obstaculos.getObstaculos2()) {
-			if (tiempo.getContar() == 600/* 0.015 */) {
-				suero.setX(Math.random() * (50 - 750) + 750);
-				suero.setY(Math.random() * (50 - 550) + 550);
-				suero.setRadio(40);
-//				suero.distancia2(suero.getX(), suero.getY(), obs.getX(), obs.getY());
-//				if (suero.colisiona(obs.getRadio(), suero.getRadio(), suero.getDistancia2())) {
-				if (suero.getX() <= obs.getX() && suero.getY() <= obs.getY()) {
-					suero.setX(suero.getX() + 40);
-					suero.setY(suero.getY() + 40);
-				} else if (suero.getX() >= obs.getX() && suero.getY() >= obs.getY()) {
-					suero.setX(suero.getX() + 40);
-					suero.setY(suero.getY() + 40);
-				} else if (suero.getX() >= obs.getX() && suero.getY() <= obs.getY()) {
-					suero.setX(suero.getX() + 40);
-					suero.setY(suero.getY() - 40);
-				} else if (suero.getX() <= obs.getX() && suero.getY() >= obs.getY()) {
-					suero.setX(suero.getX() - 40);
-					suero.setY(suero.getY() + 40);
+			if (this.tiempo.getContar() == 1000) {
+				this.suero.setX(Math.random() * (50 - 750) + 750);
+				this.suero.setY(Math.random() * (50 - 550) + 550);
+				this.suero.setRadio(40);
+				if (this.suero.getX() <= obs.getX() && this.suero.getY() <= obs.getY()) {
+					this.suero.setX(this.suero.getX() + 40);
+					this.suero.setY(this.suero.getY() + 40);
+				} else if (this.suero.getX() >= obs.getX() && this.suero.getY() >= obs.getY()) {
+					this.suero.setX(this.suero.getX() + 40);
+					this.suero.setY(this.suero.getY() + 40);
+				} else if (this.suero.getX() >= obs.getX() && this.suero.getY() <= obs.getY()) {
+					this.suero.setX(this.suero.getX() + 40);
+					this.suero.setY(this.suero.getY() - 40);
+				} else if (this.suero.getX() <= obs.getX() && this.suero.getY() >= obs.getY()) {
+					this.suero.setX(this.suero.getX() - 40);
+					this.suero.setY(this.suero.getY() + 40);
 				}
 			}
-			if (tiempo.getContar() == 2000) {
+			if (tiempo.getContar() == 3000) {
 				this.item = false;
-				suero.setX(Math.random() * (50 - 750) + 750);
-				suero.setY(Math.random() * (50 - 550) + 550);
-				suero.setRadio(40);
-				if (suero.getX() <= obs.getX() && suero.getY() <= obs.getY()) {
-					suero.setX(suero.getX() + 40);
-					suero.setY(suero.getY() + 40);
-				} else if (suero.getX() >= obs.getX() && suero.getY() >= obs.getY()) {
-					suero.setX(suero.getX() + 40);
-					suero.setY(suero.getY() + 40);
-				} else if (suero.getX() >= obs.getX() && suero.getY() <= obs.getY()) {
-					suero.setX(suero.getX() + 40);
-					suero.setY(suero.getY() - 40);
-				} else if (suero.getX() <= obs.getX() && suero.getY() >= obs.getY()) {
-					suero.setX(suero.getX() - 40);
-					suero.setY(suero.getY() + 40);
+				this.suero.setX(Math.random() * (50 - 750) + 750);
+				this.suero.setY(Math.random() * (50 - 550) + 550);
+				this.suero.setRadio(40);
+				if (this.suero.getX() <= obs.getX() && this.suero.getY() <= obs.getY()) {
+					this.suero.setX(this.suero.getX() + 40);
+					this.suero.setY(this.suero.getY() + 40);
+				} else if (this.suero.getX() >= obs.getX() && this.suero.getY() >= obs.getY()) {
+					this.suero.setX(this.suero.getX() + 40);
+					this.suero.setY(this.suero.getY() + 40);
+				} else if (this.suero.getX() >= obs.getX() && this.suero.getY() <= obs.getY()) {
+					this.suero.setX(this.suero.getX() + 40);
+					this.suero.setY(this.suero.getY() - 40);
+				} else if (this.suero.getX() <= obs.getX() && this.suero.getY() >= obs.getY()) {
+					this.suero.setX(this.suero.getX() - 40);
+					this.suero.setY(this.suero.getY() + 40);
 				}
 			}
 		}
@@ -284,107 +275,111 @@ public class Juego extends InterfaceJuego {
 	}
 
 	public void funcionSuero() {
-		if ((tiempo.getContar() > 600 && tiempo.getContar() < 1500) && !item) {
-			suero.distancia(suero.getX(), suero.getY(), mikasa.getX(), mikasa.getY());
-			if (suero.getDistancia() <= 60
-					&& suero.colisiona(mikasa.getRadio(), suero.getRadio(), suero.getDistancia())) {
+		if ((this.tiempo.getContar() > 1000 && this.tiempo.getContar() < 2000) && !this.item) {
+			this.suero.distancia(this.suero.getX(), this.suero.getY(), this.mikasa.getX(), this.mikasa.getY());
+			if (this.suero.getDistancia() <= 60
+					&& this.suero.colisiona(mikasa.getRadio(), suero.getRadio(), suero.getDistancia())) {
 				this.item = true;
-				mikasa.setConvertir(true);
-				mikasa.setRadio(105);
-				mikasa.dibujarSprite(entorno);
-				suero.setImage(null);
+				this.mikasa.setConvertir(true);
+				this.mikasa.setRadio(105);
+				this.mikasa.dibujarSprite(entorno);
+				this.suero.setImage(null);
 
 			} else {
-				suero.dibujarSprite(entorno);
+				this.suero.dibujarSprite(entorno);
 			}
-		} else if ((tiempo.getContar() > 2000 && tiempo.getContar() < 4500) && !item) {
-			suero.distancia(suero.getX(), suero.getY(), mikasa.getX(), mikasa.getY());
-			if (suero.getDistancia() <= 60
-					&& suero.colisiona(mikasa.getRadio(), suero.getRadio(), suero.getDistancia())) {
+		} else if ((this.tiempo.getContar() > 3000 && this.tiempo.getContar() < 4000) && !this.item) {
+			this.suero.distancia(this.suero.getX(), this.suero.getY(), this.mikasa.getX(), this.mikasa.getY());
+			if (this.suero.getDistancia() <= 60
+					&& this.suero.colisiona(this.mikasa.getRadio(), this.suero.getRadio(), this.suero.getDistancia())) {
 				this.item = true;
-				suero.setAux(null);
-				mikasa.setConvertir(true);
-				mikasa.setRadio(105);
-				mikasa.dibujarSprite(entorno);
+				this.suero.setAux(null);
+				this.mikasa.setConvertir(true);
+				this.mikasa.setRadio(105);
+				this.mikasa.dibujarSprite(entorno);
 
 			} else {
-				suero.dibujarSprite(entorno);
+				this.suero.dibujarSprite(entorno);
 			}
 		}
 	}
 
 	private void procesarDisparos() {
 		this.mikasa.getDisparos().forEachNodo(disparo -> {
-			if (disparo.getElemento().getImpacto()==false) {
+			if (disparo.getElemento().getImpacto() == false) {
 				disparo.getElemento().dibujarSprite(this.entorno);
 				disparo.getElemento().mover();
 			}
-			if (disparo.getElemento().getImpacto()==true || !disparo.getElemento().estaEnPantalla()) {
+			if (disparo.getElemento().getImpacto() == true || !disparo.getElemento().estaEnPantalla()) {
 				this.disparos.quitarPorId(disparo.getId());
 			}
 			return null;
 		});
 	}
-	
+
 	private void crearTitan() {
-		if (this.tickUltimoTitan == -1 || this.tickUltimoTitan + 50 < tiempo.getContar()) {
-			this.titanes.agregarAtras(new Titan(Math.random() * (100 - 700) + 700, Math.random() * (50 - 500) + 500, 120, 0));
-			this.tickUltimoTitan = (int) tiempo.getContar();
+		if (this.tickUltimoTitan == -1 || this.tickUltimoTitan + 50 < this.tiempo.getContar()) {
+			this.titanes.agregarAtras(
+					new Titan(Math.random() * (100 - 700) + 700, Math.random() * (50 - 500) + 500, 120, 0));
+			this.tickUltimoTitan = (int) this.tiempo.getContar();
 		}
 	}
 
 	private void procesarTitan() {
 		this.titanes.forEachNodo(nodoTitan -> {
 			colisionTitanes(nodoTitan);
-			if (nodoTitan.getElemento().getImpacto()==false) {
+			if (nodoTitan.getElemento().getImpacto() == false) {
 				nodoTitan.getElemento().direccionTitan(this.mikasa.getX(), this.mikasa.getY());
-				nodoTitan.getElemento().dibujar(this.entorno);
-				nodoTitan.getElemento().moverTitan();}
-			if (nodoTitan.getElemento().getImpacto()==true) {
+				nodoTitan.getElemento().dibujarSprite(this.entorno);
+				nodoTitan.getElemento().moverTitan();
+			}
+			if (nodoTitan.getElemento().getImpacto() == true) {
 				this.titanes.quitarPorId(nodoTitan.getId());
 			}
 			return null;
 		});
 	}
-	
+
 	private void impactoTitan(Nodo<Titan> titan) {
 		this.disparos.forEachNodo(disparo -> {
 			Disparo dis = disparo.getElemento();
-			dis.distancia(dis.getX(),dis.getY(),titan.getElemento().getX(), titan.getElemento().getY());
-			if (titan.getElemento().colisiona(titan.getElemento().getRadio(),dis.getRadio(), dis.getDistancia()) && dis.getDistancia() < 60) {
+			dis.distancia(dis.getX(), dis.getY(), titan.getElemento().getX(), titan.getElemento().getY());
+			if (titan.getElemento().colisiona(titan.getElemento().getRadio(), dis.getRadio(), dis.getDistancia())
+					&& dis.getDistancia() < 60) {
 				dis.setImpacto(true);
 				titan.getElemento().setImpacto(true);
-				this.puntos+=15;
-				this.eliminados+=1;
+				this.puntos += 15;
+				this.eliminados += 1;
 			}
 			return null;
 		});
 	}
-	
+
 	private void impactoObs(Obstaculos2 obs) {
 		this.disparos.forEachNodo(disparo -> {
 			Disparo dis = disparo.getElemento();
-			dis.distancia(dis.getX(),dis.getY(),obs.getX(), obs.getY());
-			if (dis.colisiona(obs.getRadio(),dis.getRadio(), dis.getDistancia()) && dis.getDistancia() < 50) {
+			dis.distancia(dis.getX(), dis.getY(), obs.getX(), obs.getY());
+			if (dis.colisiona(obs.getRadio(), dis.getRadio(), dis.getDistancia()) && dis.getDistancia() < 50) {
 				dis.setImpacto(true);
 			}
 			return null;
 		});
 	}
-	
+
 	private void colisionTitanes(Nodo<Titan> t2) {
-		this.titanes.forEachNodo(titan ->{ 
-			Titan t=titan.getElemento();
-			Titan otro=t2.getElemento();
-			if (titan.getId()!=t2.getId()) {
-				t.distancia(t.getX(),t.getY(),otro.getX(), otro.getY());
-				if (t.colisiona(t.getRadio(),otro.getRadio(), t.getDistancia()) && t.getDistancia()<60){
+		this.titanes.forEachNodo(titan -> {
+			Titan t = titan.getElemento();
+			Titan otro = t2.getElemento();
+			if (titan.getId() != t2.getId()) {
+				t.distancia(t.getX(), t.getY(), otro.getX(), otro.getY());
+				if (t.colisiona(t.getRadio(), otro.getRadio(), t.getDistancia()) && t.getDistancia() < 60) {
 					t.rebotar(t, otro);
 				}
 			}
 			return null;
 		});
 	}
+
 	public static void main(String[] args) {
 		Juego juego = new Juego();
 	}
