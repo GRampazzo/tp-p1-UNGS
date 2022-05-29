1699dc0d9c278a7ccc30adb97052cfc4c029983a
GRampazzo <gastonrampazzo@gmail.com>
2022-05-29 11:31:18 -0300

    Remove Titan2

--- TP-Progra/src/juego/Titan.java
+++ TP-Progra/src/juego/Titan.java
@@ -1,21 +1,46 @@
 package juego;
 
+import java.awt.Color;
+import java.awt.Image;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+
 public class Titan {
-	public Titan2[] titan2;
-	private double distancia;
+	double x;
+	double y;
+	double radio;
+	double angulo;
+	double distancia;
+	Image image = Herramientas.cargarImagen("titan2.png");
+	private boolean impacto=false;
 
-	public Titan(int tamanio) {
-		this.titan2 = new Titan2[tamanio];
+	public Titan(double x, double y, double radio, double angulo) {
+		this.x = x;
+		this.y = y;
+		this.radio = radio;
+		this.angulo = angulo;
 	}
 
-	public void generarTitan() {
-		for (int i = 0; i < titan2.length; i++) {
-			if (this.titan2[i] == null) {
-				this.titan2[i] = new Titan2(Math.random() * (100 - 700) + 700, Math.random() * (50 - 500) + 500, 120, 0);
-			}
-		}
+	public void dibujar(Entorno e) {
+		e.dibujarCirculo(this.x, this.y, this.radio, Color.RED);
 	}
 
+	public void dibujar2(Entorno e) {
+		e.dibujarImagen(image, x, y, 0, 0.15);
+	}
+
+	public void moverTitan() {
+		this.x -= Math.cos(this.angulo) * 0.5;
+		this.y -= Math.sin(this.angulo) * 0.5;
+	}
+
+	public void direccionTitan(double x, double y) {
+		double dx = this.x - x;
+		double dy = this.y - y;
+		this.angulo = Math.atan2(dy, dx);
+	}
+	
 	public boolean colisiona(double radio1, double radio2, double dist) {
 		return (radio1 + radio2) > dist;
 	}
@@ -23,21 +48,56 @@ public class Titan {
 	public void distancia(double x1, double y1, double x2, double y2) {
 		this.distancia = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 	}
-
+	
 //	getters and setters
-	public Titan2[] getTitan2() {
-		return titan2;
+	public double getX() {
+		return x;
 	}
 
-	public void setTitan2(Titan2[] titan2) {
-		this.titan2 = titan2;
+	public void setX(double x) {
+		this.x = x;
 	}
 
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getRadio() {
+		return radio;
+	}
+	
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+
+	public double getAngulo() {
+		return angulo;
+	}
+
+	public void setAngulo(double angulo) {
+		this.angulo = angulo;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
 	public double getDistancia() {
-		return distancia;
+		return this.distancia;
+	}
+	public boolean getImpacto() {
+		return impacto;
+	}
+	public void setImpacto(boolean a) {
+		this.impacto=a;
 	}
 
-	public void setDistancia(double distancia) {
-		this.distancia = distancia;
-	}
 }
