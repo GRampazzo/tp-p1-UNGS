05c97feaf34aead01344b57b9d94f641123d3c8c
KevDtto <104948198+KevDtto@users.noreply.github.com>
2022-05-29 16:49:29 -0300

    Update Suero.java

--- TP-Progra/src/juego/Suero.java
+++ TP-Progra/src/juego/Suero.java
@@ -10,6 +10,7 @@ public class Suero {
 	private double y;
 	private double radio;
 	private double distancia;
+	private double distancia2;
 	private Image image = Herramientas.cargarImagen("Suero.png");
 	private Image aux = this.image;
 
@@ -17,19 +18,14 @@ public class Suero {
 		this.radio = radio;
 	}
 
-//	public void dibujar(Entorno e) {
-//		e.dibujarCirculo(this.x, this.y, this.radio, Color.CYAN);
-//	}
-
 	public void dibujarSprite(Entorno e) {
 		if (this.image != null) {
-			e.dibujarImagen(image, x, y, 0, 0.1);
+			e.dibujarImagen(this.image, this.x, this.y, 0, 0.1);
 		} else if (this.aux != null) {
-			e.dibujarImagen(aux, x, y, 0, 0.1);
+			e.dibujarImagen(this.aux, this.x, this.y, 0, 0.1);
 		} else {
 			return;
 		}
-
 	}
 
 	public boolean colisiona(double radio1, double radio2, double dist) {
@@ -39,7 +35,6 @@ public class Suero {
 	public void distancia(double x1, double y1, double x2, double y2) {
 		this.distancia = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 	}
-
 
 //	getters and setters
 	public double getX() {
@@ -89,5 +84,13 @@ public class Suero {
 	public void setAux(Image aux) {
 		this.aux = aux;
 	}
-	
+
+	public double getDistancia2() {
+		return distancia2;
+	}
+
+	public void setDistancia2(double distancia2) {
+		this.distancia2 = distancia2;
+	}
+
 }
